63d63d0ef782a2cf902ba4cc07c0aaaa1d2d62b9
Yash-Ainapure <ainapureyash2@gmail.com>
2025-08-31 16:35:11 +0530

    frontend deployment

--- frontend/src/pages/DriverDetail.tsx
+++ frontend/src/pages/DriverDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getDates, getSessionsByDate } from '../api/routesApi'
 import type { Session } from '../types'
