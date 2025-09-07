ddc1c7e6cd7652ed03159e0f86636253e668722f
Yash-Ainapure <ainapureyash2@gmail.com>
2025-09-07 17:09:56 +0530

    switched toproduction apis

--- mobile2/src/api/config.js
+++ mobile2/src/api/config.js
@@ -1,7 +1,7 @@
 // Centralized API configuration
-// const API_BASE_URL = 'https://walstar-task.onrender.com/api';
+const API_BASE_URL = 'https://walstar-task.onrender.com';
 
-const API_BASE_URL = 'http://192.168.1.20:5001';
+// const API_BASE_URL = 'http://192.168.1.20:5001';
 
 
 
